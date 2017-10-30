0d6b31ef52141f5cde29b17bf03cdc21d598c001
cristianodalfarra <cristiano.dalfarra@bakeca.it>
2017-10-30 12:32:19 +0100

    test1

---
+++ DFP_script1.bat
@@ -0,0 +1,2 @@
+cd c:\users\utente\documents\
+"C:\Program Files\R\R-3.3.3\bin\x64\Rscript.exe" "C:\Users\Utente\Documents\R projects\RDFP\dfpr.R"
--- dfpR.R
+++ dfpR.R
@@ -7,6 +7,9 @@
 #***** ADX DFP migration https://developers.google.com/doubleclick-publishers/docs/adx_reporting_migration
 
 
+# ############## dashboard: https://datastudio.google.com/reporting/0B8_ub-Gf21e-cG1uQV9GQnFzdzQ/page/M7xI
+
+
 #devtools::install_github("ReportMort/rdfp")
 
 library("rdfp")
@@ -21,19 +24,19 @@ library(formattable)
 # 
 # ######################################################################
 # # how to create oauth token for DFP and GDrive
-#
+# 
 # file.remove('.httr-oauth') # Remove current token
 # library(httr)
 # 
 # oauth2.0_token(
 #   endpoint = oauth_endpoints("google"),
 #   app = oauth_app(
-#     "google", 
-#     key = getOption("rdfp.client_id"), 
+#     "google",
+#     key = getOption("rdfp.client_id"),
 #     secret = getOption("rdfp.client_secret")
 #   ),
 #   scope = c(
-#     "https://spreadsheets.google.com/feeds", 
+#     "https://spreadsheets.google.com/feeds",
 #     "https://www.googleapis.com/auth/dfp",
 #     "https://www.googleapis.com/auth/drive"),
 #   use_oob = FALSE,
@@ -41,7 +44,7 @@ library(formattable)
 # )
 # token <- dfp_auth(cache = TRUE)
 # saveRDS(token, file = "DFP_token.rds")
-# #########################################################################
+#########################################################################
 
 
 
@@ -59,8 +62,17 @@ today <- Sys.Date()
 
 yesterday_day = format(as.Date(today-1,format="%Y-%m-%d"), "%d")
 yesterday_month = format(as.Date(today-1,format="%Y-%m-%d"), "%m")
+yesterday_year = format(as.Date(today-1,format="%Y-%m-%d"), "%y")
+
+
+
+ieri = paste("20",yesterday_year,yesterday_month,yesterday_day,sep="")
+ieri_check=today-1
+
+
 
 request_data <- list(reportJob=list(reportQuery=list(dimensions='AD_EXCHANGE_TAG_NAME',
+                                                     dimensions='DATE',
                                                      adUnitView='TOP LEVEL',
                                                      columns='AD_EXCHANGE_REQUESTS',
                                                      columns="AD_EXCHANGE_MATCHED_QUERIES",
@@ -75,14 +87,41 @@ request_data <- list(reportJob=list(reportQuery=list(dimensions='AD_EXCHANGE_TAG
                                                      dateRangeType='CUSTOM_DATE')))
 
 report_data_yesterday <- dfp_full_report_wrapper(request_data)
-report_data_yesterday[9]= report_data_yesterday[9]/10^6
+report_data_yesterday[10]= report_data_yesterday[10]/10^6
 
 #last_week
 lastweek_day = format(as.Date(today-8,format="%Y-%m-%d"), "%d")
 lastweek_month = format(as.Date(today-8,format="%Y-%m-%d"), "%m")
 
+sett_scorsa= paste("2017",lastweek_month,lastweek_day,sep="")
+lastweek_check = today-8
+
+# creazioen df con le date ottenute dai report DFP
+dfp_yesterday = report_data_yesterday[which.max(report_data_yesterday$Dimension.DATE), ][2]
+dfp_lastweek = report_data_lastweek[which.max(report_data_lastweek$Dimension.DATE), ][2]
+date_from_dfp = NULL
+date_from_dfp = data.frame(dfp_yesterday,dfp_lastweek,ieri_check,lastweek_check)
+
+date_1=as.Date(paste(date_from_dfp$Dimension.DATE), "%Y-%m-%d")
+date_2=as.Date(paste(date_from_dfp$Dimension.DATE.1), "%Y-%m-%d") 
+
+if (date_1==date_from_dfp[3] && date_2==date_from_dfp[4] ) {
+  check_date="report aggiornato"
+} else {
+  check_date="report da aggiornare"
+}
+
+date_from_dfp = data.frame(date_from_dfp,check_date)
+
+
+
+ieri = paste("20",yesterday_year,yesterday_month,yesterday_day,sep="")
+ieri_check=today-1
+
+
 
 request_data <- list(reportJob=list(reportQuery=list(dimensions='AD_EXCHANGE_TAG_NAME',
+                                                     dimensions='DATE',
                                                      adUnitView='TOP LEVEL',
                                                      columns='AD_EXCHANGE_REQUESTS',
                                                      columns="AD_EXCHANGE_MATCHED_QUERIES",
@@ -97,7 +136,7 @@ request_data <- list(reportJob=list(reportQuery=list(dimensions='AD_EXCHANGE_TAG
                                                      dateRangeType='CUSTOM_DATE')))
 
 report_data_lastweek <- dfp_full_report_wrapper(request_data)
-report_data_lastweek[9]= report_data_lastweek[9]/10^6
+report_data_lastweek[10]= report_data_lastweek[10]/10^6
 
 #last_2_weeks
 last_2_weeks_day = format(as.Date(today-15,format="%Y-%m-%d"), "%d")
@@ -105,7 +144,9 @@ last_2_weeks_month = format(as.Date(today-15,format="%Y-%m-%d"), "%m")
 
 
 request_data_last_2_weeks <- list(reportJob=list(reportQuery=list(dimensions='AD_EXCHANGE_TAG_NAME',
+                                                                  dimensions='DATE',
                                                      adUnitView='TOP LEVEL',
+                                                     
                                                      columns='AD_EXCHANGE_REQUESTS',
                                                      columns="AD_EXCHANGE_MATCHED_QUERIES",
                                                      columns="AD_EXCHANGE_COVERAGE",
@@ -119,7 +160,7 @@ request_data_last_2_weeks <- list(reportJob=list(reportQuery=list(dimensions='AD
                                                      dateRangeType='CUSTOM_DATE')))
 
 report_data_last_2_weeks <- dfp_full_report_wrapper(request_data_last_2_weeks)
-report_data_last_2_weeks[9]= report_data_last_2_weeks[9]/10^6
+report_data_last_2_weeks[10]= report_data_last_2_weeks[10]/10^6
 
 
 #merge ieri e settimana scora
@@ -127,38 +168,37 @@ a= merge(report_data_yesterday,report_data_lastweek, by="Dimension.AD_EXCHANGE_T
 
 
 # colonna variazione
-a[18]=a[9]/a[17]-1
+a[20]=a[10]/a[19]-1
 
 # valori sopra e sotto 15%
-valori_15=subset(a, (a[18] >0.15 | a[18] < -0.15))
-valori_15[18]= format(round(valori_15[18], 2), nsmall = 2)
-casi = valori_15[,c(1,18)]
-casi_all = a[,c(1,18)]
+valori_15=subset(a, (a[20] >0.15 | a[20] < -0.15))
+valori_15[20]= format(round(valori_15[20], 2), nsmall = 2)
+casi = valori_15[,c(1,20)]
+casi_all = a[,c(1,20)]
 
 #merge ieri e 2 settimana fa
 b= merge(report_data_yesterday,report_data_last_2_weeks, by="Dimension.AD_EXCHANGE_TAG_NAME")
 
 
 # colonna variazione
-b[18]=b[9]/b[17]-1
+
+b[20]=b[10]/b[19]-1
 
 # valori sopra e sotto 15%
-valori_15=subset(b, (b[18] >0.15 | b[18] < -0.15))
-valori_15[18]= format(round(valori_15[18], 2), nsmall = 2)
-casi = valori_15[,c(1,18)]
-casi_all_2_weeks = b[,c(1,18)]
-
-
+valori_15=subset(a, (b[20] >0.15 | b[20] < -0.15))
+valori_15[20]= format(round(valori_15[20], 2), nsmall = 2)
+casi = valori_15[,c(1,20)]
+casi_all = b[,c(1,20)]
 
 
 #calcolo revenue 
-s_somma_scorsa_sett=sum(report_data_lastweek[9])
+s_somma_scorsa_sett=sum(report_data_lastweek[10])
 somma_scorsa_sett = format(round(s_somma_scorsa_sett, 2), nsmall = 2)
 
-s_somma_scorsa_2_sett=sum(report_data_last_2_weeks[9])
+s_somma_scorsa_2_sett=sum(report_data_last_2_weeks[10])
 somma_scorsa_2_sett = format(round(s_somma_scorsa_2_sett, 2), nsmall = 2)
 
-s_somma_ieri=sum(report_data_yesterday[9])
+s_somma_ieri=sum(report_data_yesterday[10])
 somma_ieri = format(round(s_somma_ieri, 2), nsmall = 2)
 
 #calcolo variazione 
@@ -196,7 +236,13 @@ gap <- gap %>%
 gap <- gap %>%
   gs_edit_cells(ws = "dfp_somme", input = somme.data, trim = TRUE)
 
+gap <- gap %>%
+  gs_edit_cells(ws = "date_dfp", input = date_from_dfp, trim = TRUE)
 
 
-
-
+# 
+# this_result <- dfp_getSavedQueriesByStatement(list(filterStatement=list(query="WHERE id = 10034904587")), as_df=F)
+# this_report_query <- this_result$rval$results$reportQuery
+# 
+# report_request_data2 <- list(reportJob=list(reportQuery = this_report_query))
+# delivery_dat2 <- dfp_full_report_wrapper(report_request_data2)
