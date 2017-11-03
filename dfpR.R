30d64392cfe98c61ca90dce02e7e294692df9c45
cristianodalfarra <cristiano.dalfarra@bakeca.it>
2017-11-03 11:53:09 +0100

    token rev

--- dfpR.R
+++ dfpR.R
@@ -13,6 +13,7 @@
 library(rdfp)
 library(magrittr)
 library(formattable)
+library(httr)
 # options(stringsAsFactors = FALSE)
 # options(rdfp.network_code = "4507451")
 # options(rdfp.application_name = "MyAppDFP")
@@ -53,7 +54,61 @@ options(rdfp.network_code = "4507451",
         rdfp.client_secret = "fyqqSUzmlCFcpA-pZxlT0wOB",
         rdfp.httr_oauth_cache = TRUE)
 
-dfp_auth(token = "DFP_token.rds")
+# non necessario richiamare il file con il token
+#dfp_auth(token = "DFP_token.rds")
+
+
+
+##############################################
+#https://github.com/ReportMort/rdfp/issues/4
+##############################################
+
+auth_google_services <- function(services = c('sheets', 
+                                              'sites', 
+                                              'drive', 
+                                              'analytics', 
+                                              'dfp'),
+                                 key = key, 
+                                 secret = secret)
+{
+  
+  which_services <- match.arg(services, several.ok = TRUE)
+  
+  supported_scopes <- c(sheets = "https://spreadsheets.google.com/feeds", 
+                        sites = "https://sites.google.com/feeds/", 
+                        drive = "https://www.googleapis.com/auth/drive", 
+                        analytics = "https://www.googleapis.com/auth/analytics", 
+                        dfp = "https://www.googleapis.com/auth/dfp")
+  
+  myapp <- oauth_app("google", 
+                     key = key, 
+                     secret = secret)
+  
+  google_endpoint <- oauth_endpoints("google")
+  
+  new_token <- oauth2.0_token(google_endpoint,myapp,scope = unname(supported_scopes[which_services]))
+  
+  assign("token", new_token, envir=rdfp:::.state)
+  
+  # Some packages, like RGoogleAnalytics requires you to 
+  # pass the token directly, so we'll return it invisibly
+  invisible(new_token)
+}
+
+token <- auth_google_services(services=c('drive', 'sheets', 'dfp'), 
+                              key='430054733100-o7vbb1eujetulrh6nk1gipfpmu8v6dpo.apps.googleusercontent.com', 
+                              secret='fyqqSUzmlCFcpA-pZxlT0wOB')
+##############################################################################################
+
+
+dfp_auth(token)
+
+
+
+
+
+
+
 
 
 
